e496098dd07005f00595f93c7bd03825af2a863b
Dandelion Mané <decentralion@dandelion.io>
2019-08-11 17:40:09 +0200

    add card layout to index

--- src/index.js
+++ src/index.js
@@ -35,39 +35,39 @@ const settings: { [string]: RenderSettings } = deepFreeze({
   sectionDataDriven: {
     pupil: 0.35,
     rayWidth: 0.65,
-    nRays: 40,
+    nRays: 70,
     backgroundColor: "#20364a",
     baseColor: "#ffbc95",
     midColor: "#e7a59a",
     edgeColor: "#87738c",
-    computes: [spiral(4), spiral(4), spiral(4)],
+    computes: [spiral(2), spiral(2), spiral(2)],
     weights: [
       { fixed: 0, variable: 1 },
-      { fixed: 0, variable: 2 },
-      { fixed: 2, variable: 1 }
+      { fixed: 0, variable: 1 },
+      { fixed: 0, variable: 1 }
     ],
     reverse: false
   },
   sectionTransparent: {
     pupil: 0.35,
-    rayWidth: 0.7,
-    nRays: 12,
+    rayWidth: 0.2,
+    nRays: 60,
     backgroundColor: "#20364a",
     baseColor: "#ffbc95",
     midColor: "#e7a59a",
     edgeColor: "#87738c",
-    computes: [spiral(6), spiral(3), spiral(4)],
+    computes: [spiral(-30), spiral(-15), spiral(-30)],
     weights: [
-      { fixed: 1, variable: 0 },
-      { fixed: 1, variable: 0 },
-      { fixed: 1, variable: 0 }
+      { fixed: 1, variable: 1 },
+      { fixed: 1, variable: 1 },
+      { fixed: 1, variable: 1 }
     ],
     reverse: false
   },
   sectionExtensible: {
     pupil: 0.35,
     rayWidth: 0.5,
-    nRays: 40,
+    nRays: 80,
     backgroundColor: "#20364a",
     baseColor: "#ffbc95",
     midColor: "#e7a59a",
@@ -76,6 +76,22 @@ const settings: { [string]: RenderSettings } = deepFreeze({
     weights: [
       { fixed: 0, variable: 1 },
       { fixed: 0, variable: 1 },
+      { fixed: 0, variable: 2 }
+    ],
+    reverse: false
+  },
+  sectionCommunity: {
+    pupil: 0.35,
+    rayWidth: 0.85,
+    nRays: 77,
+    backgroundColor: "#20364a",
+    baseColor: "#ffbc95",
+    midColor: "#e7a59a",
+    edgeColor: "#87738c",
+    computes: [sin(3), sin(7, Math.PI), sin(7)],
+    weights: [
+      { fixed: 1, variable: 0 },
+      { fixed: 0, variable: 2 },
       { fixed: 0, variable: 1 }
     ],
     reverse: false
@@ -83,14 +99,14 @@ const settings: { [string]: RenderSettings } = deepFreeze({
   sectionDecentralized: {
     pupil: 0.45,
     rayWidth: 1,
-    nRays: 20,
+    nRays: 80,
     backgroundColor: "#20364a",
     baseColor: "#ffbc95",
     midColor: "#e7a59a",
     edgeColor: "#87738c",
-    computes: [spiral(4), sin(4), sin(4, Math.PI)],
+    computes: [sin(4), sin(4, Math.PI), spiral(4)],
     weights: [
-      { fixed: 0.5, variable: 0 },
+      { fixed: 0.5, variable: 1 },
       { fixed: 0, variable: 1 },
       { fixed: 0, variable: 1 }
     ],
@@ -99,16 +115,16 @@ const settings: { [string]: RenderSettings } = deepFreeze({
   sectionIntersubjective: {
     pupil: 0.35,
     rayWidth: 0.8,
-    nRays: 40,
+    nRays: 120,
     backgroundColor: "#20364a",
     baseColor: "#ffbc95",
     midColor: "#e7a59a",
     edgeColor: "#87738c",
-    computes: [cos(8), cos(4), cos(8)],
+    computes: [cos(6), cos(7), spiral(6)],
     weights: [
-      { fixed: 1, variable: 1 },
-      { fixed: 1, variable: 1 },
-      { fixed: 1, variable: 1 }
+      { fixed: 1, variable: 0 },
+      { fixed: 0, variable: 2 },
+      { fixed: 0, variable: 3 }
     ],
     reverse: false
   }
@@ -126,12 +142,26 @@ function Section(props: {| +settings: RenderSettings, +children: any |}) {
   );
 }
 
+function Card(props: {|
+  +settings: RenderSettings,
+  title: string,
+  children: any
+|}) {
+  return (
+    <div className="card">
+      <MiniLogo size={128} settings={props.settings} />
+      <h3>{props.title}</h3>
+      {props.children}
+    </div>
+  );
+}
+
 export class Landing extends React.Component<{}> {
   render() {
     return (
       <div className="container">
         <canvas id="logo-canvas"></canvas>
-        <h1>SourceCred</h1>
+        <h1 id="topline">SourceCred</h1>
         <h2 className="no-bottom-margin">a reputation protocol</h2>
         <h2 className="no-top-margin">for open collaboration</h2>
 
@@ -141,42 +171,51 @@ export class Landing extends React.Component<{}> {
         </p>
         <p>Everyone who participates in the project earns cred.</p>
 
-        <Section settings={settings.sectionDataDriven}>Data Driven</Section>
-        <p>
-          Every contribution is counted, from the biggest redesign to the
-          smallest typo fix. Scores are calculated using the [PageRank
-          algorithm].
-        </p>
+        <div className="card-container">
+          <Card settings={settings.sectionDataDriven} title={"Data Driven"}>
+            <p>
+              Scores are calculated using data from a project: forum posts,
+              commits, comments, and more.
+            </p>
+          </Card>
 
-        <Section settings={settings.sectionIntersubjective}>
-          Intersubjective
-        </Section>
-        <p>
-          No algorithm can capture all the nuances of what makes a project
-          successful. That's why everyone in the community helps set priorities,
-          recognize important contributions, and calibrate the weights.
-        </p>
+          <Card
+            settings={settings.sectionCommunity}
+            title="Community Controlled"
+          >
+            <p>
+              Every instance is controlled by the project's community. They set
+              the values, priorities, and the weights.
+            </p>
+          </Card>
 
-        <Section settings={settings.sectionTransparent}>Transparent</Section>
-        <p>
-          You can always see what the cred scores, and, more importantly, how
-          they were calculated.
-        </p>
+          <Card settings={settings.sectionTransparent} title="Transparent">
+            <p>You can see what every score is, and more importantly, why.</p>
+          </Card>
 
-        <Section settings={settings.sectionExtensible}>Extensible</Section>
-        <p>
-          Out of the box, SourceCred operates on data from GitHub. It's built
-          around a plugin system, so you can extend it to track any
-          contributions you can imagine.
-        </p>
+          <Card settings={settings.sectionExtensible} title="Extensible">
+            <p>
+              SourceCred is built around a plugin architecture. You can extend
+              it to track any kind of contribution.
+            </p>
+          </Card>
 
-        <Section settings={settings.sectionDecentralized}>
-          Decentralized
-        </Section>
-        <p>
-          There's no "CredHub"; just an open-source system that anyone can setup
-          and use.
-        </p>
+          <Card settings={settings.sectionDecentralized} title="Decentralized">
+            <p>
+              There's no "CredHub"; just an open-source system that anyone can
+              setup and use.
+            </p>
+          </Card>
+          <Card
+            settings={settings.sectionIntersubjective}
+            title="Intersubjective"
+          >
+            <p>
+              SourceCred mixes objective data about contributions with
+              subjective human judgements of what matters, and why.
+            </p>
+          </Card>
+        </div>
       </div>
     );
   }
